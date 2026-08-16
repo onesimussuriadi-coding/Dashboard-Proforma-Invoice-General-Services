84b2d844f244843a655b81d9db0064beed1f01c7
onesimussuriadi-coding <onesimus.suriadi@gmail.com>
2026-08-16 16:43:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -884,7 +884,7 @@ elif modul_pilihan == "📄 Modul 2: Invoice & Dokumen Turunan":
                 """
                 st.components.v1.html(print_script, height=50)
 
-           with col_btn2:
+            with col_btn2:
                 b64_pdf = base64.b64encode(html_content.encode()).decode()
                 download_link = f'<a href="data:text/html;base64,{b64_pdf}" download="{doc_type.replace(" ", "_")}_{t_data["PI No."].replace("/", "-")}.html" style="text-decoration: none;"><button style="width: 100%; background-color: #3b82f6; color: white; padding: 10px 20px; border: none; border-radius: 6px; font-weight: bold; cursor: pointer;">📥 Download Dokumen (Format PDF/HTML)</button></a>'
                 st.markdown(download_link, unsafe_allow_html=True)
